163aa7e6c5db995a182a881152be0fa3434c3b83
Jason Jordan <jlj5aj@virginia.edu>
2021-06-28 18:03:31 -0400

    default to dev if config snac environment is omitted or invalid

--- snac/common/snac_environment.rb
+++ snac/common/snac_environment.rb
@@ -2,18 +2,23 @@ module SnacEnvironment
 
   class SNACEnvironmentException < StandardError; end
 
+  SNAC_ENV_ALPHA = :alpha
+  SNAC_ENV_DEV = :dev
+  SNAC_ENV_PROD = :prod
+  SNAC_ENV_DEFAULT = SNAC_ENV_DEV
+
   SNAC_ENV_MAPPINGS = {
-    :alpha => {
+    SNAC_ENV_ALPHA => {
       :web_url    => 'http://snac-dev.iath.virginia.edu/alpha/',
       :api_url    => 'http://snac-dev.iath.virginia.edu/alpha/rest/api/',
       :search_url => 'https://snac-dev.iath.virginia.edu/alpha/www/search'
     },
-    :dev => {
+    SNAC_ENV_DEV => {
       :web_url    => 'http://snac-dev.iath.virginia.edu/',
       :api_url    => 'http://snac-dev.iath.virginia.edu/api/',
       :search_url => 'https://snac-dev.iath.virginia.edu/search'
     },
-    :prod => {
+    SNAC_ENV_PROD => {
       :web_url    => 'https://snaccooperative.org',
       :api_url    => 'https://api.snaccooperative.org/',
       :search_url => 'https://snaccooperative.org/search'
@@ -40,8 +45,15 @@ module SnacEnvironment
 
 
   def self.get_env
-    snac_env = AppConfig[:snac_environment].to_sym
-    raise SNACEnvironmentException.new("invalid SNAC environment: [#{snac_env}]") unless SNAC_ENV_MAPPINGS.key?(snac_env)
+    if AppConfig.has_key?(:snac_environment)
+      # use supplied value if valid, otherwise fallback to default environment
+      snac_env = AppConfig[:snac_environment].to_sym
+      snac_env = SNAC_ENV_DEFAULT unless SNAC_ENV_MAPPINGS.key?(snac_env)
+    else
+      # no value supplied, use default environment
+      snac_env = SNAC_ENV_DEFAULT
+    end
+
     snac_env
   end
 end
