568661020cb6a0d47a183ca18c56560660d7e235
Jason Jordan <jlj5aj@virginia.edu>
2021-07-21 20:47:03 -0400

    export via post only

--- snac/frontend/routes.rb
+++ snac/frontend/routes.rb
@@ -4,7 +4,7 @@ ArchivesSpace::Application.routes.draw do
     match '/plugins/snac' => 'snac#index', :via => [:get]
     match '/plugins/snac/search' => 'snac#search', :via => [:get]
     match '/plugins/snac/import' => 'snac#import', :via => [:post]
-    match 'agents/:agent_type/:id/export' => 'snac#export', :via => [:get, :post]
-    match 'resources/:id/export' => 'snac#export', :via => [:get, :post]
+    match 'agents/:agent_type/:id/export' => 'snac#export', :via => [:post]
+    match 'resources/:id/export' => 'snac#export', :via => [:post]
   end
 end
