e2c76fff838a27af2c85bff328a9208696e84ac7
Jason Jordan <jlj5aj@virginia.edu>
2021-09-28 16:06:56 -0400

    fix routes to match default plugin name when cloned/unzipped into plugins dir

--- frontend/routes.rb
+++ frontend/routes.rb
@@ -1,9 +1,9 @@
 # Modified by SNAC
 ArchivesSpace::Application.routes.draw do
   scope AppConfig[:frontend_proxy_prefix] do
-    match '/plugins/snac' => 'snac#index', :via => [:get]
-    match '/plugins/snac/search' => 'snac#search', :via => [:get]
-    match '/plugins/snac/import' => 'snac#import', :via => [:post]
+    match '/plugins/snac_aspace_plugin' => 'snac#index', :via => [:get]
+    match '/plugins/snac_aspace_plugin/search' => 'snac#search', :via => [:get]
+    match '/plugins/snac_aspace_plugin/import' => 'snac#import', :via => [:post]
     match 'agents/:agent_type/:id/export' => 'snac#export', :via => [:post]
     match 'agents/:agent_type/:id/link' => 'snac#link', :via => [:post]
     match 'agents/:agent_type/:id/unlink' => 'snac#unlink', :via => [:post]
